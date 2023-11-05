3cc853a6212b21e3f7cafe6353432d4d579be175
WajahatKanju <wajahat5ahmad@gmail.com>
2023-11-05 20:40:28 +0500

    inital

---
+++ routes/users.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./db.js");
+const registered_users = express.Router();
+
+let users = [{"username":"wajahat","password":"123456789"}];
+
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+  const userMatches = users.filter((user) => user.username === username);
+  return userMatches.length > 0;
+}
+
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+  const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
+}
+
+
+
+//only registered users can login
+registered_users.post("/login", (req, res) => {
+  //Write your code here
+  console.log("login: ", req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
+});
+
+registered_users.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  // console.log(req.body)
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  if (isValid(username)) {
+    return res.status(400).json({ message: 'Username already exists' });
+  }
+
+  // Add the new user to the users array
+  users.push({ username, password });
+  return res.status(200).json({ message: 'User successfully registered' });
+});
+
+
+// Add a book review
+registered_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.session.authorization.username;
+  console.log("add review: ", req.params, req.body, req.session);
+  if (books[isbn]) {
+    let book = books[isbn];
+    book.reviews[username] = review;
+    return res.status(200).send("Review successfully posted");
+  }
+  else {
+    return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
+});
+
+registered_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+    let book = books[isbn];
+    delete book.reviews[username];
+    return res.status(200).send("Review successfully deleted");
+  }
+  else {
+    return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
+});
+
+
+
+module.exports.authenticated = registered_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
